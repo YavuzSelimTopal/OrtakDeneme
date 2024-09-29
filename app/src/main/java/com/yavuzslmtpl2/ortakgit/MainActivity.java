49497f86ec0b6a06ef4c11d8e0ff7cb5980fa044
YavuzSelim <yavuzselimtopal80@gmail.com>
2024-09-29 12:22:42 +0300

    ikinci commit

--- app/src/main/java/com/yavuzslmtpl2/ortakgit/MainActivity.java
+++ app/src/main/java/com/yavuzslmtpl2/ortakgit/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
